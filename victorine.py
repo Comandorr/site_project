26eec7655d11e9138d9b7900c8ab6c65c9e0d2da
Comandorr <Plane135790@gmail.com>
2023-05-07 19:43:39 +0300

    Улучшенная текстовая викторина

--- victorine.py
+++ victorine.py
@@ -4,7 +4,13 @@ from random import shuffle
 conn = sqlite3.connect('victorines.db')
 cursor = conn.cursor()
 
-quiz_nomer = int(input('Выберите номер викторины (от 1 до 3)'))
+cursor.execute('''SELECT name FROM quiz''')
+info = cursor.fetchall()
+for i in range(len(info)):
+    print(i+1, '-', info[i][0])
+
+
+quiz_nomer = int(input('\nВыберите номер викторины (от 1 до 3)\n'))
 
 command = '''
 SELECT question.text, question.answer1, question.answer2, question.answer3, question.answer4
@@ -14,7 +20,11 @@ WHERE links.quiz_id == ? AND question.id == links.question_id'''
 cursor.execute(command, [quiz_nomer])
 info = cursor.fetchall()
 
+right = 0
+wrong = 0
+
 for i in info:
+    print('\n')
     print(i[0])
     answers = [i[1], i[2], i[3], i[4]]
     shuffle(answers)
@@ -22,7 +32,17 @@ for i in info:
     print('-', answers[1])
     print('-', answers[2])
     print('-', answers[3])
-    print('\n')
+
+    ask = input('\nКакой ответ правильный?\n')
+    if ask == i[1]:
+        print('ПРАВИЛЬНО!')
+        right += 1
+    else:
+        print('ОШИБКА!')
+        wrong += 1
+
+print('\nВы прошли викторину и набрали', right,
+      'правильных ответов и', wrong, 'ошибок!')
 
 cursor.close()
 conn.close()
