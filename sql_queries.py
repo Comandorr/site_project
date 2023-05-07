e2119f7bd9feecaf26ffe2a98a010994e75f0e80
Comandorr <Plane135790@gmail.com>
2023-05-07 18:30:36 +0300

    Добавлены комментарии

--- sql_queries.py
+++ sql_queries.py
@@ -1,7 +1,9 @@
 import sqlite3
+# Подключение к файлу с базой данных
 conn = sqlite3.connect('victorines.db')
 cursor = conn.cursor()
 
+# Очистка базы данных (если этот файл уже запускался раньше)
 cursor.execute('''DROP TABLE IF EXISTS quiz''')
 conn.commit()
 cursor.execute('''DROP TABLE IF EXISTS question''')
@@ -9,9 +11,11 @@ conn.commit()
 cursor.execute('''DROP TABLE IF EXISTS links''')
 conn.commit()
 
+# Создание таблицы для викторин
 cursor.execute('''CREATE TABLE IF NOT EXISTS quiz (id INTEGER PRIMARY KEY, name TEXT)''')
 conn.commit()
 
+# Создание таблицы для вопросов
 cursor.execute('''CREATE TABLE IF NOT EXISTS  question (
 	id INTEGER PRIMARY KEY,
 	text TEXT,
@@ -21,6 +25,9 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS  question (
 	answer4 TEXT)''')
 conn.commit()
 
+# Создание таблицы для связей между викторинами и вопросами
+# В одной викторине может быть сразу несколько вопросов
+# Один вопрос может быть сразу в нескольких викторинах
 cursor.execute('''PRAGMA foreign_keys = on''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS  links (
 	id INTEGER PRIMARY KEY,
@@ -31,9 +38,11 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS  links (
 	)''')
 conn.commit()
 
+# Добавление новой викторины в базу данных
 cursor.execute('''INSERT INTO quiz (name) VALUES ('Первая викторина')''')
 conn.commit()
 
+# Добавление нового вопроса в базу данных
 cursor.execute('''INSERT INTO question (text, answer1, answer2, answer3, answer4)
 	VALUES('Что делает команда print', 
 		'печатает информацию', 
@@ -43,10 +52,12 @@ cursor.execute('''INSERT INTO question (text, answer1, answer2, answer3, answer4
 	''')
 conn.commit()
 
+# Добавление вопроса в викторину
 cursor.execute('''INSERT INTO links (quiz_id, question_id) VALUES (1, 1)''')
 conn.commit()
 
-cursor.execute('''SELECT * FROM links''')
+# Вывод всей информации из таблицы (название таблицы указано после FROM)
+cursor.execute('''SELECT * FROM question''')
 info = cursor.fetchall()
 print(info)
 
